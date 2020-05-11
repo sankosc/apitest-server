056abeafb0284b61283cdf19ced018bece2df74e
nozaki <nozaki@sankosc.co.jp>
2020-05-11 12:42:17 +0900

    AuthController作成

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["middleware" => "api"], function () {
+    Route::post('regist', 'Api\AuthController@regist');
+    Route::post('login', 'Api\AuthController@login');
+    Route::group(['middleware' => ['jwt.auth']], function () {
+        Route::post('logout', 'Api\AuthController@logout');
+        Route::post('refresh', 'Api\AuthController@refresh');
+        Route::post('me', 'Api\AuthController@me');
+    });
 });
